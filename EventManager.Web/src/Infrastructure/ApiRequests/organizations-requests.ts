1dd13020454c6e21097da23feb5be5e731159f00
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2024-12-05 20:02:27 +0200

    initial fronend organization requests

---
+++ EventManager.Web/src/Infrastructure/ApiRequests/organizations-requests.ts
@@ -0,0 +1,43 @@
+import { callApi } from '~/Infrastructure/api-client';
+import type {
+  OrganizationView,
+  PrimaryKeyResponse,
+  UserOrganizationView,
+} from '~/Infrastructure/api-types';
+
+export function getAllOrganizationsView() {
+  return callApi<OrganizationView[]>(`/organizations`, 'GET');
+}
+
+export function updateOrganization(
+  organizationId: number,
+  organization: FormData
+) {
+  return callApi<PrimaryKeyResponse>(
+    `/organizations/${organizationId}/update`,
+    'PUT',
+    organization
+  );
+}
+
+export function createOrganization(organization: FormData) {
+  return callApi<PrimaryKeyResponse>(
+    `/organizations/new`,
+    'POST',
+    organization
+  );
+}
+
+export function subscribeUserToOrganization(organizationId: number) {
+  return callApi<UserOrganizationView>(
+    `/organizations/${organizationId}/subscription`,
+    'POST'
+  );
+}
+
+export function unsubscribeUserFromOrganization(organizationId: number) {
+  return callApi<PrimaryKeyResponse>(
+    `/organizations/${organizationId}/subscription`,
+    'DELETE'
+  );
+}
