eaac98476b8e601b9f5ebb494041c6ebc653b1b3
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2024-12-23 14:27:25 +0200

    some script changes

--- 01_GeneratePocos.ps1
+++ 01_GeneratePocos.ps1
@@ -1,15 +1,25 @@
-# Define the path to your C# console application executable
-$exePath = "D:\dev\Tools\DbToPocoGenerator\bin\Debug\net8.0\DbToPocoGenerator.exe"
+# Save the current location
+$currentLocation = Get-Location
 
+# Define the path to your project folder
+$projectPath = "..\..\dev\Tools\DbToPocoGenerator"
+
+# Define the arguments
 $arguments = @(
     "Server=localhost;Port=5432;Database=ihelp;UserId=postgres;Password=peanutbutterjelly",
     "EventManager.DAL",
-    "D:\\dev-prod\\EventManager\\EventManager.DAL\\Pocos.cs",
-    "D:\\dev-prod\\EventManager\\EventManager.DAL\\QueryableTables.cs"
+    "D:\dev-prod\EventManager\EventManager.DAL\Pocos.cs",
+    "D:\dev-prod\EventManager\EventManager.DAL\QueryableTables.cs"
 )
 
-
+# Join the arguments into a single string
 $argumentsString = $arguments -join " "
 
-# Start the process with the executable path and arguments
-Start-Process -FilePath $exePath -ArgumentList $argumentsString -NoNewWindow -Wait
+# Navigate to the project folder
+Set-Location -Path $projectPath
+
+# Start the project with the arguments
+Start-Process -FilePath "dotnet" -ArgumentList "run -- $argumentsString" -NoNewWindow -Wait
+
+# Return to the original location
+Set-Location -Path $currentLocation
--- 02_GenerateWebTypes.ps1
+++ 02_GenerateWebTypes.ps1
@@ -1,2 +1,17 @@
-dotnet build .\EventManager.TypescriptGenerator\EventManager.TypescriptGenerator.csproj
-& .\EventManager.TypescriptGenerator\bin\Debug\net8.0\EventManager.TypescriptGenerator.exe
+# Save the current location
+$currentLocation = Get-Location
+
+# Define the path to the project folder
+$projectPath = ".\EventManager.TypescriptGenerator"
+
+# Define the command to start the project
+$startCommand = "dotnet run"
+
+# Navigate to the project folder
+Set-Location -Path $projectPath
+
+# Execute the start command
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c $startCommand" -NoNewWindow -Wait
+
+# Return to the original location
+Set-Location -Path $currentLocation
